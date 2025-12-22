21a71e5d210d43893ba0f495c71df9a3e3d558e7
leemseungwon <leemsh1236@gmail.com>
2025-12-22 12:28:35 +0900

    Feat : Main UI

---
+++ Assets/01.Develop/LSW/01. Scripts/UI/FixScrollBarValue.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _01.Develop.LSW._01._Scripts.UI
+{
+    [RequireComponent(typeof(ScrollRect))]
+    public class FixScrollBarValue : MonoBehaviour
+    {
+        [Range(0f, 1f)]
+        public float targetValue = 0f;
+
+        private ScrollRect _scrollRect;
+
+        void OnEnable()
+        {
+            _scrollRect = GetComponent<ScrollRect>();
+
+            Canvas.ForceUpdateCanvases();
+
+            if (_scrollRect.verticalScrollbar != null)
+                _scrollRect.verticalScrollbar.value = targetValue;
+
+            if (_scrollRect.horizontalScrollbar != null)
+                _scrollRect.horizontalScrollbar.value = targetValue;
+
+            _scrollRect.verticalNormalizedPosition = targetValue;
+            _scrollRect.horizontalNormalizedPosition = targetValue;
+        }
+
+    }
+}
