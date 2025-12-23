e37029c4f2030a17a8790a0eb31484ec41300553
dongho <jijiji0809160@gmail.com>
2025-12-23 12:29:19 +0900

    transition update

---
+++ Assets/01.Develop/CDH/Code/Core/TransitionManager.cs
@@ -0,0 +1,21 @@
+﻿using _01.Develop.LSW._01._Scripts.Manager;
+using _01.Develop.LSW._01._Scripts.UI.InGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._01.Develop.CDH.Code.Core
+{
+    public class TransitionManager : MonoSingleton<TransitionManager>
+    {
+        [SerializeField] private DiagonalStarTransition transition;
+
+        public void NextScene(string sceneName)
+        {
+            transition.Play(sceneName);
+        }
+    }
+}
