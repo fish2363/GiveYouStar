3bf1290587ca5bde903030f0ea47d6a0e5d99635
leemseungwon <leemsh1236@gmail.com>
2025-12-23 20:58:01 +0900

    Fix : Build Error

--- Assets/01.Develop/CDH/Code/Ropes/ChargingDataSO.cs
+++ Assets/01.Develop/CDH/Code/Ropes/ChargingDataSO.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UnityEditor.Experimental.Rendering;
 using UnityEngine;
 
 namespace Assets._01.Develop.CDH.Code.Ropes
