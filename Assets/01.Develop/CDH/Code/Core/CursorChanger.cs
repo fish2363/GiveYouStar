8035286f27866a59cb31ef22feab218ebf956e91
dongho <jijiji0809160@gmail.com>
2025-12-23 21:43:38 +0900

    icon, cursor

---
+++ Assets/01.Develop/CDH/Code/Core/CursorChanger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CursorChanger : MonoBehaviour
+{
+    [SerializeField] private Texture2D cursorTexture; // 인스펙터에서 넣기
+    [SerializeField] private Vector2 hotSpot = Vector2.zero; // 커서의 클릭 포인트 (보통 중심이나 왼쪽 위)
+    [SerializeField] private CursorMode cursorMode = CursorMode.Auto;
+
+    void Start()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+}
