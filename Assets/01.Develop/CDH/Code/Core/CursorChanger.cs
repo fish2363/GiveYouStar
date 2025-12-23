916ea6f2b3c1d5a4d087a048892ee92d932b9d04
dongho <jijiji0809160@gmail.com>
2025-12-23 22:18:30 +0900

    update:

--- Assets/01.Develop/CDH/Code/Core/CursorChanger.cs
+++ Assets/01.Develop/CDH/Code/Core/CursorChanger.cs
@@ -6,7 +6,7 @@ public class CursorChanger : MonoBehaviour
     [SerializeField] private Vector2 hotSpot = Vector2.zero; // 커서의 클릭 포인트 (보통 중심이나 왼쪽 위)
     [SerializeField] private CursorMode cursorMode = CursorMode.Auto;
 
-    void Start()
+    private void Start()
     {
         Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
     }
